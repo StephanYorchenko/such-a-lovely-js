2abd45a76d80de1f7af67f74b2de118bcafaee06
StephanYorchenko <yurchenko_s@tochka.com>
2021-04-19 23:08:14 +0500

    Добавил заготовку под авторизацию

--- src/server.js
+++ src/server.js
@@ -2,41 +2,28 @@ const express = require('express')
 const session = require('express-session')
 const store = require('session-file-store')(session)
 
+const app = express();
+const port = 31337;
 
-const app = express()
-const port = 31337
-
-app.use(express.json())
-app.use(express.static(__dirname + 'public'))
+app.use(express.static(__dirname + 'public'));
 app.use(session({
     store: new store(),
-    secret: "send_goge",
-}))
-
-const GitHubStrategy = require('passport-github').Strategy;
-
-passport.use(new GitHubStrategy({
-        clientID: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: "/auth/github/callback"
-    },
-    function(accessToken, refreshToken, profile, cb) {
-        User.findOrCreate({ githubId: profile.id }, function (err, user) {
-            return cb(err, user);
-        });
-    }
-));
+    secret: process.env["SECRET_KEY"],
+}));
+app.use(express.urlencoded({ extended: true }))
 
 function checkCredentials(login){
-    return login === "asdasd";
+    return login === "asdasd"; // TODO: add auth;
 }
 
 app.post('/tryLogin', (req, res) =>{
-    console.log(req)
-    req.session.isLogin = checkCredentials('wertyuio');
-    if (req.session.isLogin)
+    req.session.isLogin = checkCredentials(req.body.login);
+    if (req.session.isLogin) {
         res.redirect('/');
-    res.send({success: false});
+    }
+    else {
+        res.send({success: false});
+    }
 })
 
 app.get('/', (req, res) => {
