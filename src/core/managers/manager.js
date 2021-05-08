ec38bcafda8af8e96713d6698554e91eedac2f4b
StephanYorchenko <yurchenko_s@tochka.com>
2021-05-08 22:10:18 +0500

    Немного бекенда + протокол общения между частями

---
+++ src/core/managers/manager.js
@@ -0,0 +1,33 @@
+class HandlerFactory{
+    createHandler(methodName, func){
+        const handler = {};
+        handler[methodName] = func;
+        return handler;
+    }
+}
+
+class Manager{
+    constructor() {
+        this.handlers = [];
+    }
+
+    addHandler(handler){
+        this.handlers.push(handler);
+    }
+
+    tryExecute(methodName, args, user){
+        console.log(methodName, args)
+        for (const handler of this.handlers)
+            if (Object.getOwnPropertyNames(handler).includes(methodName))
+                return handler[methodName](args, user);
+    }
+}
+
+const handlers = new HandlerFactory();
+const manager = new Manager();
+
+//set handlers to methods
+manager.addHandler(handlers.createHandler("listCreatedSurveys", (_, user) => ['aaa', user]));
+
+
+module.exports = Object.freeze(manager)
---
+++ src/ui/widgets/loginWidget.js
@@ -0,0 +1,20 @@
+import {BaseWidget} from "./baseWidget";
+
+class LoginWidget extends BaseWidget{
+    _generateHTML() {
+        const form = document.createElement('form');
+        form.action = "tryLogin";
+        form.method = "post";
+        const label = document.createElement("label");
+        label.for = "login";
+        const login = document.createElement("input");
+        login.name = "login";
+        label.append(login);
+        label.append("Логин");
+        form.append(label);
+        const button = document.createElement("button");
+        button.innerText = "Вход";
+        form.append(button);
+        return form;
+    }
+}
