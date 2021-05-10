95230a0f70fca374c95f61b40ee0a3ff34ecd2ad
lololozhkin <alexand3r.lozhkin@gmail.com>
2021-05-11 00:52:13 +0500

    docker-compose with postgress added

---
+++ src/pgtest.js
@@ -0,0 +1,18 @@
+const { Client } = require('pg')
+const client = new Client({
+    user: 'postgres',
+    password: 'password',
+    database: 'demo',
+})
+
+async function main() {
+    await client.connect()
+    const res = await client.query('SELECT * FROM aircrafts LIMIT 5;')
+    for (row of res.rows) {
+        console.log(row)
+    }
+    // console.log(res.rows[0].message) // Hello world!
+    await client.end()
+}
+
+main()
