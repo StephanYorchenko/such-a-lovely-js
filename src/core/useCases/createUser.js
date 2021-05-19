bd025a26884ed5404b64ee506ca87599a8572f88
lololozhkin <alexand3r.lozhkin@gmail.com>
2021-05-20 00:44:31 +0500

    merge edited

--- src/core/useCases/createUser.js
+++ src/core/useCases/createUser.js
@@ -2,8 +2,8 @@ const BaseUseCase = require('./baseUseCase');
 const UserRepository = require('../../infrastructure/repositories/userRepository');
 
 class CreateUserUseCase extends BaseUseCase{
-	static execute(params, request){
-		const result = UserRepository.createUser(params.userName);
+	static async execute(params, request){
+		const result = await UserRepository.createUser(params.userName);
 		request.session.user = result || undefined;
 		console.log(request.session.user, result);
 		request.session.isLogin = true;
