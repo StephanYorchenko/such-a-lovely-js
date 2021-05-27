c967c79b00d8c73beb856059ea8f03a31aafc68a
StephanYorchenko <stepan33314@yandex.ru>
2021-05-27 20:12:05 +0500

    лИнТерР

--- src/core/useCases/createUser.js
+++ src/core/useCases/createUser.js
@@ -4,11 +4,11 @@ const UserRepository = require('../../infrastructure/repositories/userRepository
 class CreateUserUseCase extends BaseUseCase{
 	static execute(params, request){
 		if (params.userName === '')
-			return {success: false, error: "Пустое имя пользователя"};
+			return {success: false, error: 'Пустое имя пользователя'};
 		const result = UserRepository.createUser(params.userName);
 		request.session.user = result || undefined;
 		request.session.isLogin = result;
-		return {success: Boolean(result), error: "Данное имя уже занято"};
+		return {success: Boolean(result), error: 'Данное имя уже занято'};
 	}
 }
 
