6b81042641bfefbc63a0aa61f46da3cc07ebd716
StephanYorchenko <stepan33314@yandex.ru>
2021-05-29 00:27:39 +0500

    фиксики

--- src/core/useCases/createUser.js
+++ src/core/useCases/createUser.js
@@ -5,7 +5,7 @@ class CreateUserUseCase extends BaseUseCase{
 	static async execute(params, request){
 		if (params.userName === '')
 			return {success: false, error: 'Пустое имя пользователя'};
-		const result = UserRepository.createUser(params.userName);
+		const result = await UserRepository.createUser(params.userName);
 		request.session.user = result || undefined;
 		request.session.isLogin = Boolean(result);
 		return {success: Boolean(result), error: 'Данное имя уже занято'};
